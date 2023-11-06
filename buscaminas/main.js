ca94aad4a0ca537608a716e24b2e057c6e26e485
Daniel Cuesta <danicu988@gmail.com>
2023-11-06 12:16:17 +0100

    añadiendo ejercicios

--- buscaminas/main.js
+++ buscaminas/main.js
@@ -1,7 +1,7 @@
 // VARIABLES GLOBALES
-let filas = 15;
-let cols = 15;
-let lado = 20; // Lado de cada casilla
+let filas = 10;
+let cols = 10;
+let lado = 25; // Lado de cada casilla
 
 function generarJuego() {
     generarTablero();
@@ -12,7 +12,7 @@ function generarTablero() {
     for (let f = 0; f < filas; f++) {
         tabla += `<tr>`;
         for (let c = 0; c < cols; c++) {
-            tabla += `<td id="celda-${c}-${f}" style="width:${lado}px; height:${lado}px"></td>`;
+            tabla += `<td id="celda-${c}-${f}" style="width:${lado}px; height:${lado}px">${f}/${c}</td>`;
         }
         tabla += "</tr>";
     }
